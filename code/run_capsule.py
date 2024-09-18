0f13e98f66ef9c155e5ae8fab70c61e5a0da0125
Arielle Leon <ariellel@alleninstitute.org>
2024-09-18 18:53:49 +0000

    Added environment.json; edited run_capsule.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,7 +1,28 @@
-""" top level run script """
+from pathlib import Path
+import json
 
-def run():
-    """ basic run function """
-    pass
+INPUT_DIR = Path("/data/")
+OUTPUT_DIR = Path("/results/")
 
-if __name__ == "__main__": run()
+if __name__ == "__main__":
+    try:
+        session_json_fp = next(INPUT_DIR.glob("session.json"))
+    except StopIteration:
+        session_json_fp = next(INPUT_DIR.glob("*/session.json"))
+    
+    with open(session_json_fp, "r") as j:
+        session_data = json.load(j)
+    pair_list = []
+    for data_stream in session_data["data_streams"]:
+        if data_stream["ophys_fovs"]:
+            for fov in data_stream["ophys_fovs"]:
+                index = fov["index"]
+                coupled_fov_index = fov["coupled_fov_index"]
+                if coupled_fov_index in pair_list:
+                    pair2_fp = INPUT_DIR / str(fov["targeted_structure"] + "_" + str(index))
+                    pair_list = []
+                    with open(OUTPUT_DIR / f"pair{coupled_fov_index}.txt", "w") as f:
+                        f.write(f"{pair1_fp}\n{pair2_fp}\n")
+                else:
+                    pair1_fp = INPUT_DIR /  str(fov["targeted_structure"] + "_" + str(index))
+                    pair_list.append(coupled_fov_index)
